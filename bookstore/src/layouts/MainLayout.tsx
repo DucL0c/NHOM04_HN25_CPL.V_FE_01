b39189cbca1b2f474ca551e32ff706fef4305bcf
LEHAIDANG <dangvn2003@gmail.com>
2025-08-09 22:48:10 +0700

    Header and Footer

--- bookstore/src/layouts/MainLayout.tsx
+++ bookstore/src/layouts/MainLayout.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 
 const MainLayout = () => (
-  <>
+  <div className="min-h-screen flex flex-col bg-[#efefef]">
     {/* <Header /> */}
-    <main className="min-h-screen p-4">
+    <Header />
+    {/* Main content area */}
+    <main className="min-h-screen">
       <Outlet />
     </main>
     {/* <Footer /> */}
-  </>
+    <Footer />
+  </div>
 );
 
 export default MainLayout;
