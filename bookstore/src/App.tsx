fda8ea3857e3268752385ef9be2cf8743287cd30
Duc Loc <tranducloc2010@gmail.com>
2025-08-19 23:03:02 +0700

    Bản chạy ổn 19/08/2025

--- bookstore/src/App.tsx
+++ bookstore/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <Toaster position="top-right" />
       <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<div>Loading....</div>}>
           <CartCountProvider>
             <AppRoutes />
           </CartCountProvider>
